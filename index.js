dda031acdfe4ece2219202b98a3396231e681bce
arnavozil <arnavpanwar99@gmail.com>
2020-05-29 01:52:18 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const cors = require('cors');
 
 let interval;
 
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello')
