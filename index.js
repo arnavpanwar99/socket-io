95f580b266d58a4f4822cc42b1085f7ac9429df7
arnavozil <arnavpanwar99@gmail.com>
2020-05-29 01:46:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+let interval;
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+});
+
+io.on('connection', (socket) => {
+    if(interval){
+        clearInterval(interval);
+    }
+    interval = setInterval(() => {
+        getApiAndEmit(socket);
+    }, 1000);
+    socket.on('disconnect', () => {
+        console.log('client disconnected');
+        clearInterval(interval);
+    });
+});
+
+const getApiAndEmit = socket => {
+    const response = new Date();
+
+    //emitting a new message will be consumed by the client
+    socket.emit("FromAPI", response);
+};
+
+http.listen(port, () => console.log(`server up on ${port}`));
