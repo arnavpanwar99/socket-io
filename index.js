dc90ace071ab61f06798bd69d07bed82dbbb59b9
arnavozil <arnavpanwar99@gmail.com>
2020-05-29 21:31:12 +0530

    added chat

--- index.js
+++ index.js
@@ -3,34 +3,49 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const cors = require('cors');
 
-let interval;
+const { addUser, removeUser, getUser, getUserInRoom } = require('./users');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+
+const { router } = require('./router');
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello')
-});
+io.on('connection', socket => {
+    socket.on('join', ({name, room}, callback) => {
+        const { error, user } = addUser({name, room, id: socket.id});
+        if(error){
+            return callback(error);
+        };
 
-io.on('connection', (socket) => {
-    if(interval){
-        clearInterval(interval);
-    }
-    interval = setInterval(() => {
-        getApiAndEmit(socket);
-    }, 1000);
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` });
+        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} has joined!`});
+
+        socket.join(user.room);
+
+        io.to(user.room).emit('roomData', {room: user.room, users: getUserInRoom(user.room)})
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {user: user.name, text: message});
+        io.to(user.room).emit('roomData', {room: user.room, users:getUserInRoom(user.room)});
+
+        callback();
+    })
+
     socket.on('disconnect', () => {
-        console.log('client disconnected');
-        clearInterval(interval);
+        const user = removeUser(socket.id);
+
+        if(user){
+            io.to(user.room).emit('message', {user: 'admin', text: `${user.name} has left the chat.`});
+        }
     });
 });
 
-const getApiAndEmit = socket => {
-    const response = new Date();
-
-    //emitting a new message will be consumed by the client
-    socket.emit("FromAPI", response);
-};
+app.use(router);
 
 http.listen(port, () => console.log(`server up on ${port}`));
